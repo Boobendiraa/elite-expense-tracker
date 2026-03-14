95dd51873fdf17a6242fe02336e45b15636d9bee
Boobendiraa <boobendiraa7@gmail.com>
2026-03-14 15:26:42 +0530

    Improved README and added clear all expenses feature

--- main.cjs
+++ main.cjs
@@ -14,6 +14,11 @@ function createWindow() {
   
 }
 
+function clearExpenses() {
+    localStorage.removeItem("expenses");
+    location.reload();
+}
+
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
